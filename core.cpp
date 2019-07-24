40ab17ad38ffe3121004697df168da9a22407206
Sriram P H <sriramph@Srirams-MacBook-Pro.local>
2019-07-24 22:41:27 +0530

    Initial Commit

---
+++ core.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+typedef Frame std::string;
+
+
+//TODO get input from user
+std::string getInput(){
+  return "Hero enters a car";
+}
+
+std::vector<std::string> getNouns(std::string userInput){
+  return
+}
+
+bool hasContent(Frame frame, std::string userInput){
+  //
+}
+
+char **getFrames(){
+  return nullptr;
+}
+
+void contentFound(){
+  std::cout << "Content Found" << std::endl;
+}
+
+int main(int argc, char *argv[]){
+  std::string in = getInput();
+  //extract frames and frameSize from mp4
+
+  return 0;
+}
